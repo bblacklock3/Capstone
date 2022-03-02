56887c12140ad289d6f159d69fd104c7e7a4a947
colinjstewart42 <colinjstewart42@gmail.com>
2022-03-02 14:15:36 -0500

    Plot v2

--- scripts/testing/PlotAllAngles.m
+++ scripts/testing/PlotAllAngles.m
@@ -1,18 +1,23 @@
-%% 
-clear
-clc
-close all
+%% Plot all Angles
 
+% Resetting MATLAB
+clear, clc, close;
+
+% Data set 1
 Data = readmatrix('S1_fig2_both.xlsx');
 T_Hip = 1;
 Hip = 2;
 T_Knee = 7;
 Knee = 8;
-Bounds = [0,1];
+Bounds = [1.5, 2.8];
 Fig_Num = 1;
 n = 10;
-
 [p_Hip, p_Knee] = HipKneePlots(Data, T_Hip, Hip, T_Knee, Knee, Bounds, Fig_Num, n);
+
+
+
+
+
 
 
 
