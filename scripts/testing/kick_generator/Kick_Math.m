137b2577eab36d320e15f17d72f1db6e5f75c3e9
colinjstewart42 <colinjstewart42@gmail.com>
2022-03-07 20:08:01 -0500

    Kicking generator update

--- scripts/testing/kick_generator/Kick_Math.m
+++ scripts/testing/kick_generator/Kick_Math.m
@@ -3,7 +3,38 @@
 
 
 
+vals = [174.0039    0.0251    0.1248  124.7606    0.0463    2.0604   16.2617    0.0881    2.6480    2.6931    0.2450   -3.7469    1.3434    0.2756   -2.3161    0.7008    0.3128   -1.0626    0.1503    0.4871   -0.8925    5.1932    0.1600   -1.2477];
+prep = rand(1, 24) ./ 20 + 1;
+vals = vals .* prep;
+
+a1 = vals(1);
+b1 = vals(2);
+c1 = vals(3);
+a2 = vals(4);
+b2 = vals(5);
+c2 = vals(6);
+a3 = vals(7);
+b3 = vals(8);
+c3 = vals(9);
+a4 = vals(10);
+b4 = vals(11);
+c4 = vals(12);
+a5 = vals(13);
+b5 = vals(14);
+c5 = vals(15);
+a6 = vals(16);
+b6 = vals(17);
+c6 = vals(18);
+a7 = vals(19);
+b7 = vals(20);
+c7 = vals(21);
+a8 = vals(22);
+b8 = vals(23);
+c8 = vals(24);
 
 
+x = T_Hip;
+x = linspace(0,120);
+y = a1*sin(b1*x+c1) + a2*sin(b2*x+c2) + a3*sin(b3*x+c3) + a4*sin(b4*x+c4) + a5*sin(b5*x+c5) + a6*sin(b6*x+c6) + a7*sin(b7*x+c7) + a8*sin(b8*x+c8);
 
-
+plot(x,y);
---
+++ scripts/testing/kick_generator/Polynomial_Gatherer.m
@@ -0,0 +1,102 @@
+%% Gathers polynomial for selected curve section
+
+% clear
+clc, close all
+
+X = linspace(0,100)';
+Y = linspace(0,1)';
+[p] = Polynomial_Gattherer(X, Y)
+
+function [p] = Polynomial_Gattherer(X, Y)
+p = {};
+X_Total = {X};
+Y_Total = {Y};
+lines = [];
+diff = 0.05;
+
+for i = 1:5
+    check = 'N';
+    count = 1;
+    while strcmp(check, 'Y') == 0 & strcmp(check, '') == 0
+        
+        % Plotting raw data
+        figure(100);
+        hold on;
+        for ii = 1:i
+            plot(X_Total{ii},Y_Total{ii});
+        end
+        for iii = 1:length(lines)
+            xline(lines(iii), '-.');
+        end
+        
+        % Labeling plot
+        xlabel('Time (s)');
+        ylabel('Joint Angle (Degrees)');
+        title('Infant Joint Angles vs Time');
+        
+        % Input bounds and poly degree
+        if i == 1
+            disp('Set Lower x Limit');
+            [x1, ~] = ginput(1);
+            xline(x1, '-.');
+        else
+            if count == 1
+                x1 = x2;
+                x1_original = x1;
+            else
+                x1 = x1_original;
+            end
+        end
+        disp('Set Upper x Limit');
+        [x2, ~] = ginput(1);
+        xline(x2, '-.');
+        pause(2);
+        commandwindow;
+        n = input('Input Polynomial Degree: ');
+        
+        % Slightly extending poly bounds
+        x_diff = diff .* (x2 - x1);
+        x1_p = x1 - x_diff;
+        x2_p = x2 + x_diff;
+        
+        % Trimming data
+        mask = X > x1_p & X < x2_p;
+        Trimmed_X = X(mask);
+        Trimmed_Y = Y(mask);
+        
+        % Finding best fit
+        p_i = polyfit(Trimmed_X, Trimmed_Y, n);
+        X_p = linspace(x1,x2);
+        Y_p = polyval(p_i, X_p);
+        
+        % Plotting fit
+        plot(X_p, Y_p);
+        
+        % Checking if data if correct
+        figure(100);
+        pause(3);
+        commandwindow;
+        check = input('Y/N? ', 's');
+        
+        % Closes figure
+        close(figure(100));
+        count = count + 1;
+    end
+    % Preparing output
+    p = [p; p_i];
+    X_Total = [X_Total X_p];
+    Y_Total = [Y_Total Y_p];
+    lines = [lines x1 x2];
+end
+
+% Polynomial plot
+figure(101);
+
+
+
+
+
+
+
+
+end
